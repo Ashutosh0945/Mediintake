d1c101eada32bf96b03a12a300e899ca41fa6f97
Ashutosh Singh <ashutosh.236437109@vcet.edu.in>
2026-03-28 18:34:15 +0530

    Fix intakes RLS + PWA install for all users

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,50 @@
-var CACHE = 'mediintake-v2';
+var CACHE_NAME = 'mediintake-v3';
+var URLS_TO_CACHE = ['/', '/manifest.json'];
 
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  if (e.request.method !== 'GET') return;
-  if (e.request.url.indexOf('supabase') > -1) return;
-  if (e.request.url.indexOf('chrome-extension') > -1) return;
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  if (event.request.url.indexOf('supabase') > -1) return;
+  if (event.request.url.indexOf('chrome-extension') > -1) return;
+  if (event.request.url.indexOf('fonts.googleapis') > -1) return;
 
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      var networkFetch = fetch(event.request).then(function(response) {
+        if (response && response.status === 200 && response.type !== 'opaque') {
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      });
+      return cached || networkFetch;
     })
   );
 });
